d7ae6f289e0bb69da897d30d483418986603ba94
Your Name <heron.rmei@gmail.com>
2024-02-29 09:40:50 -0300

    1 : comit

---
+++ saidadeDados.py
@@ -0,0 +1,37 @@
+#adição
+print(1+1)
+
+#subtração
+print(10-2)
+
+#multiplicação
+print(10*3)
+
+#divisão
+print(12/4)
+
+#exponenciação
+print(5 ** 2)
+
+#modulo
+print(10 % 2)
+
+print("=====calculos arimétcios com varíaveis=====")
+x = 20
+y = 5
+
+print(x/y)
+
+print("===== operadores de comparação =====")
+valor_1= 500
+valor_2= 250
+
+print(valor_1 == valor_2)
+print(valor_1 != valor_2)
+print(valor_1 >= valor_2)
+print(valor_1 <= valor_2)
+print(valor_2 >  valor_2)
+print(valor_2 < valor_2)
+
+
+
